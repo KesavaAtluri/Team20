1cc6506f189719fb4463ee669450ebf30d7d4cf0
jeetesh46 <114968964+jeetesh46@users.noreply.github.com>
2022-11-13 19:57:16 -0500

    Add files via upload

---
+++ routes/main.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const mongoCollections=require('../config/mongoCollections');
+const usersData = require('../data/main');
+const main = mongoCollections.main;
+const doctorsData = require('../data/doctors')
+const users = mongoCollections.users;
+const  xss= require('xss'); // Cross Site Scripting
+
+
+router.get('/',async(req,res)=>{
+    if(!req.session.user){
+        res.render('landingPage');
+        return;
+    }else{
+        res.redirect('search');
+        return;
+    }
+});
+
+router.get('/signup', async(req,res)=>{
+    if(!req.session.user){
+        res.render('signup');
+        return;
+    }else{
+        res.redirect('search');
+        return;
+    }
+});
+
+
+router.post('/signup',async(req,res)=>{
+    try{
+        let fname = xss(req.body.firstName);
+        let lname = xss(req.body.lastName);
+        let uname = xss(req.body.username);
+        let pnumber = xss(req.body.phoneNumber);
+        let pass = xss(req.body.password);
+        let u_dob = xss(req.body.dob);
+        let u_street = xss(req.body.street);
+        let u_city = xss(req.body.city);
+        let u_state = xss(req.body.state);
+        let u_zipcode = xss(req.body.zipCode);
+        let u_email = xss(req.body.email);
+        uname=uname.toLowerCase();
+        
+        const u_signup = await usersData.createUsers(fname,lname,u_dob, u_street,u_city,u_state,u_zipcode,pnumber,u_email,uname,pass);
+        if(u_signup){
+            res.redirect('/login');
+        }
+        }catch(e){
+            res.status(e.error||500).render('signup',{error:e||'Internal Server Error'});
+            return;
+    }
+});
+
+
+router.get('/logout',async(req,res)=>{
+    req.session.destroy();
+    res.redirect('/');
+});
+
+module.exports=router;
